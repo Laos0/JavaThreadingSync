cdde069d4301809d3c73ebac49194b7a127a1ce4
Xai <xlao@menard-inc.com>
2019-04-14 19:09:58 -0500

    update2

--- JavaMultithreadedSync/src/Wrapper.java
+++ JavaMultithreadedSync/src/Wrapper.java
@@ -4,6 +4,9 @@ public class Wrapper implements Runnable {
 
 	private ArrayList<Customer> customers;
 	private int from, to;
+
+	// cutomers with final count less than 1000
+	public int finalCount;
 	
 	Wrapper(ArrayList<Customer> customers, int from, int to){
 		this.customers = customers;
@@ -19,5 +22,16 @@ public class Wrapper implements Runnable {
 		// each wrapper will run each thread givent he to and from index.
 		// like this wrapper instance will run index 0-500
 
+	}
+
+	private void parallelCountJunkCode(int from, int to) {
+		if(customers != null && customers.size() > 0) {
+			for(Customer cust: customers) {
+				// loop
+				if(cust.getBalance() < 1000) {
+					finalCount++;
+				}
+			}
+		}
 	}
 }
